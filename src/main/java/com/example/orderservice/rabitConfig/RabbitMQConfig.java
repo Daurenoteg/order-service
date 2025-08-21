1981100f072b51a8ba6ac4d198d2c9dc8176c10c
The Dauren <BananzaSobaken@gamil.com>
2025-08-21 20:41:47 +0500

    Added Jackson2JsonMessageConverter

--- src/main/java/com/example/orderservice/rabitConfig/RabbitMQConfig.java
+++ src/main/java/com/example/orderservice/rabitConfig/RabbitMQConfig.java
@@ -1,13 +1,24 @@
 // RabbitMQConfig.java
-package com.example.orderservice.config;
+package com.example.orderservice.rabitConfig;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter());
+        return template;
+    }
+
 
     @Value("${app.rabbitmq.exchange}")
     private String exchange;
@@ -31,5 +42,10 @@ public class RabbitMQConfig {
     @Bean
     public Binding binding(Queue orderQueue, TopicExchange topicExchange) {
         return BindingBuilder.bind(orderQueue).to(topicExchange).with(routingKey);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 }
